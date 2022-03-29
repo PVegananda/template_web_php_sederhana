cc5d25ea552653f6ace5d5e01040b5844f008876
Pasyah Vegananda <pasyahwivegananda@gmail.com>
2022-03-29 22:43:39 +0700

    template web

---
+++ crud.php
@@ -0,0 +1,26 @@
+<!-- HEADER -->
+<?php include('partials/header.php'); ?>
+<!-- HEADER -->
+
+<style>
+    .main {
+        position: fixed;
+        margin-left: 200px;
+    }
+
+    .margin {
+        margin-left: 200px;
+        align-content: center;
+    }
+</style>
+
+<?php 
+$title = "crud";
+?>
+
+
+
+
+<!-- FOOTER -->
+<?php include('partials/footer.php'); ?>
+<!-- FOOTER -->
---
+++ gya.php
@@ -0,0 +1,23 @@
+<!-- HEADER -->
+<?php include('partials/header.php'); ?>
+<!-- HEADER -->
+
+<style>
+    .main {
+        position: fixed;
+        margin-left: 200px;
+    }
+
+    .margin {
+        margin-left: 200px;
+        align-content: center;
+    }
+</style>
+
+
+
+
+
+<!-- FOOTER -->
+<?php include('partials/footer.php'); ?>
+<!-- FOOTER -->
